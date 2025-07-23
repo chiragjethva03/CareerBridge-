a0dac668f480033b522bd0f9d294edbbefffb371
chiragjethva03 <chiragjethva23@gmail.com>
2025-07-23 16:20:56 +0530

    new

--- middleware.js
+++ middleware.js
@@ -1,24 +1,24 @@
 
-if (userDetails) {
-    console.log(`Current User Type: ${userType}`);
-    console.log(userDetails.applicants);
-    const applicantIds = userDetails.applicants.map(candidate => candidate.toString());
-    console.log(applicantIds);
+// if (userDetails) {
+//     console.log(`Current User Type: ${userType}`);
+//     console.log(userDetails.applicants);
+//     const applicantIds = userDetails.applicants.map(candidate => candidate.toString());
+//     console.log(applicantIds);
 
-    if (applicantIds) {
-        forms = await Form.find({ _id: { $in: applicantIds } }).exec();
-    }
-} else {
-    console.log("User not found in either collection.");
-}
+//     if (applicantIds) {
+//         forms = await Form.find({ _id: { $in: applicantIds } }).exec();
+//     }
+// } else {
+//     console.log("User not found in either collection.");
+// }
 
-let internDetailsArray = [];
-for (const form of forms) {
-    let idIntern = form.intern.toString();
-    const internDetails = await Intern.findById(idIntern);
-    internDetailsArray.push(internDetails);
-}
+// let internDetailsArray = [];
+// for (const form of forms) {
+//     let idIntern = form.intern.toString();
+//     const internDetails = await Intern.findById(idIntern);
+//     internDetailsArray.push(internDetails);
+// }
 
-const internDetails = internDetailsArray;
-console.log('Intern Details:', internDetails);
-console.log('Forms:', forms);
+// const internDetails = internDetailsArray;
+// console.log('Intern Details:', internDetails);
+// console.log('Forms:', forms);
